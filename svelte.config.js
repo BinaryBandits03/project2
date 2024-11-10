63b09b0cf56633ea94e2431e5ed553a2241c8b79
VincentYzer <202210606@gordoncollege.edu.ph>
2024-11-10 16:40:49 +0800

    Try again

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,13 @@ const config = {
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/project2' : '',
 		  },
+		  prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+			  // Prevents build failures on 404s during prerendering
+			  return false;
+			},
+			entries: ['*']
+		  },
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
